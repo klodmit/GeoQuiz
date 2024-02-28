4212158351a6baf0929a04772292dd0904320d6e
klodmit <klochixindima8@gmail.com>
2024-02-28 23:36:31 +0300

    lesson 2 I try to add Left button with drawableStart? but now this attribute doesn't work with material theme and this is awful, I think. So did this with drawableLeft
    Need to go from Button to ImageButton

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,37 +5,65 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,true),
+        Question(R.string.question_africa,true),
+        Question(R.string.question_america,true),
+        Question(R.string.question_asia,true),
+    )
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        questionTextView.setOnClickListener { view: View ->
+            currentIndex = (currentIndex+1)% questionBank.size
+            updateQuestion()
+        }
 
         trueButton.setOnClickListener { view: View ->
-            val correctToast = Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            )
-            correctToast.apply {Gravity.TOP; show() }
-
-
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            val incorrectToast = Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            )
-            incorrectToast.apply {setGravity(Gravity.TOP, 0, 0); show() }
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex+1)% questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if(userAnswer == correctAnswer){
+            R.string.correct_toast
+        } else{
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
+
